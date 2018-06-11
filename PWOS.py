63695fca1d53828a27d1ef1fb5b223235c6dbe15
cmy00cmy <chenmyuan@163.com>
2018-06-11 18:42:17 +0800

    remove zh_CN

--- PWOS.py
+++ PWOS.py
@@ -24,35 +24,10 @@ fcoin.proxies = {
     }
 fcoin.auth('key', 'secret') 
 orders = []
-hold = 0 #持仓
+hold = 0 #balance_temp
 def main():
-    lastBar = 0
-    count = 0
     buy = sell = []
-    vol = 10#默认持仓
-    actions = []#操作记录
     hold = 0
-    #print(fcoin.get_symbols())
-#    balance = fcoin.get_balance()
-#    print(balance)
-    
-#    
-#    #vol = (lambda a,b: 10 if min(a, b) > 10 else min(a, b))(dib_vol, ask_vol)
-#    print('买入:', bid, ';卖出', ask, ';数量', vol)
-##    print('买单：', fcoin.buy(pair, bid, vol))
-##    print('卖单：', fcoin.sell(pair, ask, vol * (1-fee)))
-##    
-##    orders = fcoin.list_orders()
-##    print(orders)
-#    balance = fcoin.get_balance()
-#    balances = []
-#    for data in balance['data']:
-#        temp = []
-#        for j in data:
-#            temp.append(data[j])
-#        if Decimal(temp[0]) != 0:
-#            balances.append(temp)
-#    print(balances)
     while(1):
         ohlcv = getOHLCV()
         ohlcv = ohlcv.sort_index(by='time')
@@ -61,7 +36,6 @@ def main():
         dif, dea, bar = talib.MACD(ohlcv.close, fastperiod = 12, slowperiod = 26, signalperiod = 9)
         
         print(bar.tail(10))
-        curBar = bar[0]
         if bar[0] * bar[1] < 0:
             if bar[0] < 0 and bar[1] > 0  and hold > 0:
                 orders.append(sell())
@@ -113,10 +87,10 @@ def sell():
     round(float(amount) / ask, 2)
     timeNow = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     ask = str(round(Decimal(ask), 8))
-    print(timeNow, '创建卖单，卖价', ask, '数量：', amount)
+    print(timeNow, 'Create Order:<===sell，sell_price', ask, 'amount:', amount)
     order = fcoin.sell(pair, ask, amount)
     if order.shape <= 0:
-        print('创建买单失败')
+        print('CREATE ORDER(SELL) ERROR!!')
         return []
     return order['data']
 
@@ -126,10 +100,10 @@ def buy():
     amount = round(float(amount) / bid, 2)
     timeNow = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     bid = str(round(Decimal(bid), 8))
-    print(timeNow, '创建买单，买价', bid, '数量：', amount)
+    print(timeNow, 'Create Order:===>buy，buy_price', bid, 'amount:', amount)
     order = fcoin.buy(pair, bid, amount)
     if order.shape <= 0:
-        print('创建买单失败')
+        print('CREATE ORDER(BUY) ERROR!!')
         return []
     return order['data']
 
